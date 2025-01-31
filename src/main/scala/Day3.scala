4e8b3d0ceff963d68c0597683913b0d7eeb62d56
Amizan Jaleel <najm555@gmail.com>
2025-01-30 22:32:31 -0500

    complete day 3

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -13,4 +13,39 @@ object Day3 {
     // Sum up all products
     products.sum
   }
+
+  def sumValidMultiplicationsWithDosAndDonts(input: String): Int = {
+    // Define patterns for `do()`, `don't()`, and `mul(X, Y)`
+    val doPattern = """do\(\)""".r
+    val dontPattern = """don't\(\)""".r
+    val mulPattern = """mul\((\d+),(\d+)\)""".r
+
+    // Split the input into tokens based on the "do()" and "don't()" markers
+    val tokens = input.split("""(?=do\(\)|don't\(\))""").toList // Split but retain the markers
+
+    def sumProducts(t: String) = mulPattern.findAllMatchIn(t).map { m =>
+      val x = m.group(1).toInt
+      val y = m.group(2).toInt
+      x * y
+    }.sum
+
+    tokens match {
+      case Nil => 0 // return 0 for empty case
+      case head :: tail =>
+        val initialSum = sumProducts(head)
+        // Fold through the tokens to calculate the sum
+        tail.foldLeft(initialSum) { case (sum, token) =>
+          token.trim match {
+            case t if doPattern.findFirstIn(t).isDefined =>
+              // Enable multiplication and process any mul() in the same token
+              sum + sumProducts(t)
+
+            case t if dontPattern.findFirstIn(t).isDefined =>
+              sum // Disable multiplication
+
+            case _ => sum
+          }
+        }
+    }
+  }
 }
--- src/test/scala/Day3Test.scala
+++ src/test/scala/Day3Test.scala
@@ -14,5 +14,18 @@ class Day3Test extends Specification {
         Day3.sumValidMultiplications(input) mustEqual 183788984
       }
     }
+
+    "pass part 2" in {
+      "the simple case" >> {
+        val s = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+        Day3.sumValidMultiplicationsWithDosAndDonts(s) mustEqual 48
+      }
+
+      "reading from a file" >> {
+        val input = scala.io.Source.fromResource("day3.txt").getLines().toList.mkString("")
+        Day3.sumValidMultiplicationsWithDosAndDonts(input) mustEqual 62098619
+      }
+    }
   }
 }
